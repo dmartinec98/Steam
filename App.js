8b2d18698c17d0c44f24f12fca2f569ba087f52f
Danijel Martinec <danijel.martinec98@gmail.com>
2023-07-13 20:02:43 +0200

    Bottom navigation

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import WishlistScreen from "./screens/WishlistScreen";
 import WalletScreen from "./screens/WalletScreen";
+import HeaderComponent from "./components/HeaderComponent";
+import {
+  BellIcon,
+  EllipsisVerticalIcon,
+  ShieldCheckIcon,
+  Bars3Icon,
+  NewspaperIcon,
+  HomeIcon,
+} from "react-native-heroicons/outline";
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Wishlist" component={WishlistScreen} />
-        <Stack.Screen name="Wallet" component={WalletScreen} />
-      </Stack.Navigator>
+      <HeaderComponent />
+      <Tab.Navigator
+        screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => <HomeIcon color="#000000" size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name="Wishlist"
+          component={WishlistScreen}
+          options={{
+            tabBarIcon: () => <NewspaperIcon color="#000000" size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name="Wallet"
+          component={WalletScreen}
+          options={{
+            tabBarIcon: () => <ShieldCheckIcon color="#000000" size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name="Wallet2"
+          component={WalletScreen}
+          options={{
+            tabBarIcon: () => <BellIcon color="#000000" size={20} />,
+          }}
+        />
+        <Tab.Screen
+          name="Wallet3"
+          component={WalletScreen}
+          options={{
+            tabBarIcon: () => <Bars3Icon color="#000000" size={20} />,
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,51 +7,18 @@ import {
   ScrollView,
 } from "react-native";
 import React from "react";
-import {
-  UserIcon,
-  MagnifyingGlassIcon,
-  EllipsisVerticalIcon,
-  ChevronDownIcon,
-} from "react-native-heroicons/outline";
+
+import HeaderComponent from "../components/HeaderComponent";
 import Categories from "../components/Categories";
 import FeaturedRow from "../components/FeaturedRow";
-import { useNavigation } from "@react-navigation/native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+  const Tab = createBottomTabNavigator();
 
   return (
-    <SafeAreaView className=" bg-white pt-5">
-      <View className="flex-row items-center space-x-2 pb-2 mx-4">
-        <View className="flex-row flex-1 space-x-2  bg-gray-200 p-3 items-center h-10">
-          <TextInput className="flex-1" placeholder="Search" />
-          <MagnifyingGlassIcon color="#000000" size={20} />
-        </View>
-        <EllipsisVerticalIcon color="#000000" size={20} />
-        <Image
-          source={{ uri: "https://links.papareact.com/wru" }}
-          className="h-10 w-10 bg-gray-300 p-4"
-        />
-      </View>
-      <View className="flex-row justify-center space-x-2 pb-2 mx-4 ">
-        <Text className="py-1 px-2">MENU</Text>
-        <Text
-          className="py-1 px-2"
-          onPress={() => {
-            navigation.navigate("Wishlist", {});
-          }}
-        >
-          WISHLIST
-        </Text>
-        <Text
-          className="py-1 px-2"
-          onPress={() => {
-            navigation.navigate("Wallet", {});
-          }}
-        >
-          WALLET
-        </Text>
-      </View>
+    <>
       <ScrollView
         className="bg-gray-100"
         contentContainerStyle={{
@@ -66,22 +33,8 @@ const HomeScreen = () => {
           title="Recommended"
           description="Primjer necega kaj bu priakzano"
         />
-        <View className="flex-row justify-around py-5 ">
-          <Text className="border-solid border-2 border-black rounded-full py-1 px-2">
-            NEW
-          </Text>
-          <Text className="border-solid border-2 border-black rounded-full py-1 px-2">
-            TRENDING
-          </Text>
-          <Text className="border-solid border-2 border-black rounded-full py-1 px-2">
-            TOP
-          </Text>
-          <Text className="border-solid border-2 border-black rounded-full py-1 px-2">
-            UPCOMING
-          </Text>
-        </View>
       </ScrollView>
-    </SafeAreaView>
+    </>
   );
 };
 
